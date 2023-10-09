15e1e5d03926b890333111b43e5eacf324d48946
Somesh Yadav <52493755+iamsomesh@users.noreply.github.com>
2023-10-09 11:23:42 +0000

    Javascript operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,27 @@
+let value = 3
+let negValue = -value
+
+console.log(value, negValue, typeof negValue)
+
+//js uses all arithmatic operations +, -, /, *, %
+// console.log(1+1)
+// console.log(1-1)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(3%2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + 1 + "2")
+console.log("1" + "1" + 1)
+
+str1 = "somesh"
+str2 = " yadav"
+console.log(str1+str2) //concatination of string
+
+console.log(true) //true
+console.log(+true) //1
+
+//parallel assignment
+num1 = num2 = num3 = 3
+console.log(num1, num2, num3)
