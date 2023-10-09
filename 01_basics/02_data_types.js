ee4b5eba32040fec8d48b092ecee67ea5d4ec941
Somesh Yadav <52493755+iamsomesh@users.noreply.github.com>
2023-10-09 07:31:39 +0000

    data type in javascript

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version of js
+
+// alert( 3+3 ) - we are using node js so it will gives us error here but we can use on browser
+
+// data types in js
+//integer, bigInt, string, boolean, undefined, null, symbol - premitive data types
+
+let name = "Somesh" // String
+let age = 30 // Integer
+let isLoggedIn = false //Boolean
+let surname //undefined
+let creditCardDetails = null //null  -> standalone value \
+
+// symbol - uniquness find krne ke liye
+
+//Object 
+
+console.log(typeof age, typeof name, typeof isLoggedIn, typeof "somesh") //typeof is used to find the type of variable
