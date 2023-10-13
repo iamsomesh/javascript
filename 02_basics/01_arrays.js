9eb6be46d0ef8e8b887a59b6db0e76fd5d2b6173
Somesh Yadav <syb777yadav@gmail.com>
2023-10-13 12:22:03 +0530

    Array in javascript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+//arrays
+//imp points -> js arrays are resizeable, can contain multiple data types
+//javascript array copy operation create shallow copies, rather than deep copies
+//Array decalration
+
+const myArray = [3,5,6,4,7,8] //using square bracket []
+console.log(myArray);
+
+//also we can also use combine element in single aaray
+
+const anotherArray = [true, 3, "somesh"]
+console.log(anotherArray);
+
+//another way to declare
+const arrTwo = new Array(2,3,6,"somesh", true)
+console.log(arrTwo);
+
+//properties
+//length -> not a function it is properties we can use without paranthesis ()
+
+console.log(arrTwo.length);
