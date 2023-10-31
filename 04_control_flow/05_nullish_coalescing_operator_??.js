94890ccd8ffa1a11395e99dc6e57b09294c33f0f
Somesh Yadav <syb777yadav@gmail.com>
2023-10-31 12:34:28 +0530

    Nullish coalescing operator ?? in js

--- 04_control_flow/05_nullish_coalescing_operator_??.js
+++ 04_control_flow/05_nullish_coalescing_operator_??.js
@@ -2,6 +2,7 @@
 
 // The nullish coalescing (??) operator is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
 
+
 let user; //works on null and undefined only
 
 user = null ?? "somesh"
